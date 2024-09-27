6ca142ffc22ef06cf2be9fa6e868597e62b34874
FourSeagulls <cuatrogabiotas@gmail.com>
2024-09-27 10:02:31 +0200

    Creada en raiz la ventana con 3 panelespara el menu, el titulo y el central, en el menuBar unmenu Archivo. Todo sin funcionalidad

--- Ventana.java
+++ Ventana.java
@@ -3,21 +3,57 @@ package formulario;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 
 public class Ventana extends JFrame{
-	Ventana(){
-		setSize(500,500);
-		setLocationRelativeTo(null);
-		setTitle("Calculos diversos");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public Ventana(){
+        setSize(500,500);
+        setLocationRelativeTo(null);
+        setTitle("Calculos diversos");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.blue);
-		JLabel titulo = new JLabel();
-		titulo.setText("Formulario derentes");
+        setBackground(Color.red);
+        // Crea un panel principal y aplica el BoxLayout ahí
+        JPanel panelPrincipal = new JPanel();
+        //panelPrincipal.setOpaque(true);//ESTO NO SE VE
+        panelPrincipal.setLayout(new BoxLayout(panelPrincipal, BoxLayout.Y_AXIS));
 
-		panel.add(titulo);
-		add(panel);
-	}
+        panelPrincipal.setBackground(Color.blue);
+
+        JMenuBar barraMenu = new JMenuBar();
+        JMenu menuArchivo = new JMenu("Archivo");
+        barraMenu.add(menuArchivo);
+        panelPrincipal.add(barraMenu);
+      
+        JPanel panelMenu = new JPanel();
+        panelMenu.setBackground(Color.orange);
+        panelPrincipal.add(panelMenu);
+
+        JLabel titulo = new JLabel();
+        titulo.setForeground(Color.yellow);
+        titulo.setText("<html><h1>Formulario de Gerentes</html></h1>");
+        panelPrincipal.add(titulo);
+
+        JPanel panelCentral = new JPanel();
+        panelCentral.setBackground(Color.green);
+        panelPrincipal.add(panelCentral);
+
+        // Añadir el panel principal a la ventana
+        add(panelPrincipal);
 }
+}
+
+  /*
+        
+        JTextField areaTexto = new JTextField(); // El tamaño del JTextField se ajusta con columnas, no pixeles
+        areaTexto.setSize(100,30);
+        areaTexto.setText("Cojones");
+        panelPrincipal.add(areaTexto);
+
+        */
